67becd5c6c03c473a0996fb0d7a5cef08887a6fe
unknown <ismaelsidahmed.hasnaoui@gmail.com>
2024-06-25 14:49:34 +0200

    sOMETHING

---
+++ api/webhook.js
@@ -0,0 +1,16 @@
+// pages/api/webhook.js
+
+export default function handler(req, res) {
+    const { query } = req;
+    const mode = query['hub.mode'];
+    const token = query['hub.verify_token'];
+    const challenge = query['hub.challenge'];
+    const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN; // Set this in your environment
+  
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      res.status(200).send(challenge);
+    } else {
+      res.status(403).end();
+    }
+  }
+  
