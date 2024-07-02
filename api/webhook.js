aee6f4f6c86ac3623d44629e33bf84ce61c6172f
unknown <ismaelsidahmed.hasnaoui@gmail.com>
2024-07-02 12:20:50 +0200

    Updated API Key

--- api/webhook.js
+++ api/webhook.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     const mode = query['hub.mode'];
     const token = query['hub.verify_token'];
     const challenge = query['hub.challenge'];
-    const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN; // Set this in your environment
+    const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
   
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       res.status(200).send(challenge);
