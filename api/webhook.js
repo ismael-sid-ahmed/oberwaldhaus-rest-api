d93d4aab9d2f01ef79e05153cec7d8a125721247
unknown <ismaelsidahmed.hasnaoui@gmail.com>
2024-07-02 13:05:57 +0200

    Added verification token

--- api/webhook.js
+++ api/webhook.js
@@ -5,6 +5,7 @@ export default function handler(req, res) {
     const challenge = query['hub.challenge'];
     const VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN; // Set this in your environment
     console.log('VERIFY_TOKEN from env:', process.env.MY_VERIFY_TOKEN);
+    console.log('mode:', process.env.mode);
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       res.status(200).send(challenge);
     } else {
